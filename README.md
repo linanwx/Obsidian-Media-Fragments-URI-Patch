87c0343d1f0470750834273a54a55e2b37b4d320
linanwx <linanwx@gmail.com>
2022-07-24 02:29:42 +0800

    first version

--- README.md
+++ README.md
@@ -1,73 +1,21 @@
-# Obsidian Sample Plugin
+# Obsidian Media Fragments URI Patch
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+这个插件可以让 Obsidian 中的媒体支持 Media Fragments URI 属性，该属性可以让媒体播放指定的段，或者时间戳。
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+This plugin enables the media in Obsidian to support the media fragments URI attribute, which allows the media to play the specified segment, or the timestamp.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+这个插件的使用和作用类似 media-extended ，但是我个人在使用过程中发现该插件有一些问题，因此开发了这个插件。
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+The use and function of this plugin is similar to media extended, but I personally found some problems with this plugin in the process of use, so I developed this plugin.
 
-## First time developing plugins?
+这个插件只专注于播放指定片段，尽可能的减少影响范围，因此我希望出现问题的概率更小。
 
-Quick starting guide for new plugin devs:
+This plugin only focuses on playing the specified clips, reducing the scope of influence as much as possible, so I hope there might be less problem.
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+使用下面的方式来为媒体增加片段。
 
-## Releasing new releases
+Use the following methods to add clips to the media.
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
-
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+```
+![[audio.mp3#t=1,2]]
+```
--- main.ts
+++ main.ts
@@ -1,137 +1,91 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownRenderChild,Plugin } from 'obsidian';
+import { TextFileView, WorkspaceLeaf, TFile, parseLinktext } from "obsidian"
+import filetypeinfo from 'magic-bytes.js'
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ObsidianMFUPatch {
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export const acceptedExt: Map<string, string[]> = new Map([
+	["audio", ["mp3", "wav", "m4a", "ogg", "3gp", "flac"]],
+	["video", ["mp4", "ogv"]],
+	["media", ["webm"]],
+]);
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export const getMediaType = (src: TFile): string => {
+	let ext = src.extension
+	let fileType = "";
+	for (const [type, extList] of acceptedExt) {
+		if (extList.includes(ext)) fileType = type;
+	}
+	return fileType;
+};
 
+export default class ObsidianMFUPatchPlugin extends Plugin {
+	settings: ObsidianMFUPatch;
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		this.registerMarkdownPostProcessor((element, context) => {
+			const blocks = element.querySelectorAll('span.internal-embed, div.internal-embed');
+			blocks.forEach(async (span, key, parent) => {
+				if (span instanceof HTMLElement) {
+					let src = span.getAttr("src")
+					if (src) {
+						let tmparray = src.split("#")
+						if (tmparray.length == 2) {
+							let path = parseLinktext(src).path
+							let file = app.metadataCache.getFirstLinkpathDest(path, context.sourcePath)
+							if (file) {
+								context.addChild(new FragmentPatch(span, file, tmparray[1]))
+							}
+						}
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			})
+		}, 0)
 	}
-
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+export class FragmentPatch extends MarkdownRenderChild {
+	private file: TFile
+	private fragment: string
+	private original: HTMLElement
+
+	constructor(containerEl: HTMLElement, file: TFile, fragment: string) {
+		super(containerEl);
+		this.file = file
+		this.fragment = fragment
+		this.original = containerEl
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	async onload(): Promise<void> {
+		let mediaType = getMediaType(this.file)
+		if (mediaType == "media") {
+			let array = await app.vault.readBinary(this.file)
+			let guessedFiles = filetypeinfo(new Uint8Array(array,0,100))
+			guessedFiles.forEach((guessedfile, index, array)=>{
+				if (guessedfile.mime?.includes("audio")) {
+					mediaType = "audio"
+				}
+				if (guessedfile.mime?.includes("video")) {
+					mediaType = "video"
+				}
+			})
+		}
+		let div = createDiv("div")
+		let path = app.vault.getResourcePath(this.file)
+		if (mediaType == "audio") {
+			let audio = div.createEl(mediaType)
+			audio.setAttr("controls", "")
+			audio.setAttr("src", path + "#" + this.fragment)
+			audio.setAttr("onpause", "this.load()")
+		} else if (mediaType == "video") {
+			let video = div.createEl(mediaType)
+			video.setAttr("controls", "")
+			video.setAttr("src", path + "#" + this.fragment)
+			video.setAttr("onpause", "this.load()")
+		} else {
+			return
+		}
+		this.original.replaceWith(div)
 	}
 }
